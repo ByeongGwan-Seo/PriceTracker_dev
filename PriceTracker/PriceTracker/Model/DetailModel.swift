12541cc689111036f3b5b7206da0009abf6f8cfe
Byeong Gwan Seo <seo.jp.work@gmail.com>
2023-01-23 16:02:29 +0900

    fetch detail information

---
+++ PriceTracker/PriceTracker/Model/DetailModel.swift
@@ -0,0 +1,31 @@
+//
+//  DetailModel.swift
+//  PriceTracker
+//
+//  Created by seobyeonggwan on 2023/01/23.
+//
+
+import Foundation
+
+struct DetailModel: Codable {
+    let info: Info
+    let cheapestPriceEver: CheapestPriceEver
+    let deals: [Deal]
+}
+
+struct CheapestPriceEver: Codable {
+    let price: String
+    let date: Int
+}
+
+struct Deal: Codable {
+    let storeID, dealID, price, retailPrice: String
+    let savings: String
+}
+
+// MARK: - Info
+struct Info: Codable {
+    let title: String
+    let steamAppID: String?
+    let thumb: String
+}
