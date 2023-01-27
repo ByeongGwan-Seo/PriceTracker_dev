41b5090b603a410405ca630cba37445591e004d0
Byeong Gwan Seo <seo.jp.work@gmail.com>
2023-01-27 15:57:29 +0900

    tracking Delete in CRUD

--- PriceTracker/PriceTracker/ViewController/TrackingViewController.swift
+++ PriceTracker/PriceTracker/ViewController/TrackingViewController.swift
@@ -73,7 +73,7 @@ class TrackingViewController: UIViewController {
                                 userPrice: userPrice,
                                 gameID: gameID,
                                 thumb: thumb,
-                                isTracked: true)
+                                isTracked: isTracked)
         }
     }
 }
@@ -118,6 +118,14 @@ extension TrackingViewController: UITableViewDelegate {
 extension TrackingViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return trackingList.count
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            trackingList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        saveTrackingList()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
