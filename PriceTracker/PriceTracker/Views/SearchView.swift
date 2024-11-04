cb9504b35c744a7f058ff36cc6914a453eee6eea
Byeong Gwan Seo <seo.jp.work@gmail.com>
2024-11-04 17:38:03 +0900

    1. added swiftUI SearchView 2. added UIHostingController to SearchViewController

---
+++ PriceTracker/PriceTracker/Views/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  PriceTracker
+//
+//  Created by seobyeonggwan on 2024/11/04.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("Testing converting to swift ui from uikit")
+    }
+}
+
+#Preview {
+    SearchView()
+}
