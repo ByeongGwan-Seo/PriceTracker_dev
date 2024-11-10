f69ed9654cef5f34b31fea593a7929977a90b590
Byeong Gwan Seo <seo.jp.work@gmail.com>
2024-11-10 18:27:57 +0900

    added combine in viewmodel

--- PriceTracker/PriceTracker/ViewModels/SearchViewModel.swift
+++ PriceTracker/PriceTracker/ViewModels/SearchViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 protocol SearchViewModelProtocol {
     var searchText: String { get set }
@@ -22,18 +23,41 @@ class SearchViewModel: SearchViewModelProtocol, ObservableObject {
     
     private let networkService: NetworkServiceProtocol
     private let router: RouterProtocol
+    private var cancellables: Set<AnyCancellable> = []
     
     init(
-            networkService: NetworkServiceProtocol = NetworkService(),
-            router: RouterProtocol
-        ) {
-            self.networkService = networkService
-            self.router = router
-        }
+        networkService: NetworkServiceProtocol = NetworkService(),
+        router: RouterProtocol
+    ) {
+        self.networkService = networkService
+        self.router = router
+    }
     
     func fetchGameList() {
-
-    }
+            isLoading = true
+        Future<[SearchGameList], Error> { promise in
+                Task {
+                    do {
+                        let gameList = try await self.networkService.fetchGameList(title: self.searchText)
+                        promise(.success(gameList))
+                    } catch {
+                        promise(.failure(error))
+                    }
+                }
+            }
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    self.isLoading = false
+                case .failure:
+                    self.isLoading = false
+                    print("error occured")
+                }
+            }, receiveValue: { gameList in
+                self.searchResults = gameList
+            })
+            .store(in: &cancellables)
+        }
     
     func moveToDetail() {
         router.showDetail()
