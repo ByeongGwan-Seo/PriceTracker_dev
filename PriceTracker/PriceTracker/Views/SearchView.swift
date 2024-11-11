f3e3572e4523e7d2f8e66cb3facf5bf4d01fbc4e
Byeong Gwan Seo <seo.jp.work@gmail.com>
2024-11-11 12:57:35 +0900

    deleted comment

--- PriceTracker/PriceTracker/Views/SearchView.swift
+++ PriceTracker/PriceTracker/Views/SearchView.swift
@@ -32,26 +32,6 @@ struct SearchView: View {
     }
 }
 
-//이런 식으로도 사용할 수 있음
-//                Foo(action: searchViewModel.moveToDetail)
-
-//각 컴포넌트의 라이브러리를 보고 사용할 수 있을지 여부를 판단할 수 있다.
-//struct Foo: View {
-//isLoacing:Bool
-//    private let  viewModel: SearchViewModelProtocol
-////    private let action: () -> Void
-//    
-//    init(action: @escaping () -> Void) {
-//        self.action = action
-//    }
-//    
-//    var body: some View {
-//        Button(action: {viewModel.moveToDetail()}) {
-//            Text("Hello, World!")
-//        }
-//    }
-//}
-
 struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
         class MockViewModel: SearchViewModelProtocol {
