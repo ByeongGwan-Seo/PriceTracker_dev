7baac3d56a90fe8f927119b28fcfb52c7fd5e64f
Byeong Gwan Seo <seo.jp.work@gmail.com>
2024-11-12 17:14:11 +0900

    fetching list

--- PriceTracker/PriceTracker/Views/SearchView.swift
+++ PriceTracker/PriceTracker/Views/SearchView.swift
@@ -10,22 +10,45 @@ import Combine
 
 struct SearchView: View {
     @ObservedObject var searchViewModel: SearchViewModel
-
+    
     var body: some View {
-        VStack {
-            if searchViewModel.isLoading {
-                Text("search is Loading")
-            } else {
-                
-                Button(action: searchViewModel.moveToDetail) {
-                    Text("Go to Detail View")
+        NavigationView {
+            VStack {
+                TextField("Enter game title", text: $searchViewModel.searchText)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .submitLabel(.search)
+                    .onSubmit(searchViewModel.fetchGameList)
+                    .padding(.top, 20)
+                if !searchViewModel.searchResults.isEmpty {
+                    List(searchViewModel.searchResults, id: \.gameID) { game in
+                        Text(game.external)
+                    }
+                    .listStyle(.plain)
+                } else {
+                    Text("No results found.")
                         .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
                 }
-                .padding(.top, 20)
+                Spacer()
+                
             }
         }
     }
 }
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        class MockSearchViewModel: SearchViewModel {
+            override init() {
+                super.init()
+                self.searchResults = [
+                    SearchGameList(gameID: "1", steamAppID: "1001", cheapest: "5.99", cheapestDealID: "deal_001", external: "https://game1.com", thumb: "thumb1"),
+                    SearchGameList(gameID: "2", steamAppID: "1002", cheapest: "9.99", cheapestDealID: "deal_002", external: "https://game2.com", thumb: "thumb2"),
+                    SearchGameList(gameID: "3", steamAppID: "1003", cheapest: "12.99", cheapestDealID: "deal_003", external: "https://game3.com", thumb: "thumb3")
+                ]
+            }
+        }
+        return SearchView(searchViewModel: MockSearchViewModel())
+    }
+}
+
