d79c04dacfd8e0c066274e482bb4efd45abcadd1
Byeong Gwan Seo <seo.jp.work@gmail.com>
2024-11-18 18:11:11 +0900

    seperated NoContentView as a file

---
+++ PriceTracker/PriceTracker/Views/NoContentView.swift
@@ -0,0 +1,14 @@
+//
+//  NoContentView.swift
+//  PriceTracker
+//
+//  Created by seobyeonggwan on 2024/11/18.
+//
+
+import SwiftUI
+
+struct NoContentView: View {
+    var body: some View {
+        Text("No content")
+    }
+}
