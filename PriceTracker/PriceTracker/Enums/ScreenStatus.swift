b9300dc45d16a0a27414ba8b3e2dc5b25320acf8
Byeong Gwan Seo <seo.jp.work@gmail.com>
2024-11-18 18:10:50 +0900

    added ScreenStatus enum

---
+++ PriceTracker/PriceTracker/Enums/ScreenStatus.swift
@@ -0,0 +1,13 @@
+//
+//  ScreenStatus.swift
+//  PriceTracker
+//
+//  Created by seobyeonggwan on 2024/11/18.
+//
+
+enum ScreenStatus {
+    case loading
+    case success
+    case noContent
+    case error
+}
