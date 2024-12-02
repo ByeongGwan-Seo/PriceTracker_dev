7678ade3d2fe28bcfea6494295f2dd340c71fa43
Byeong Gwan Seo <seo.jp.work@gmail.com>
2024-12-02 17:37:25 +0900

    added tracking button in detail view

--- PriceTracker/PriceTracker/Views/Detail/BasicInfoView.swift
+++ PriceTracker/PriceTracker/Views/Detail/BasicInfoView.swift
@@ -9,49 +9,45 @@ import SwiftUI
 
 struct BasicInfoView: View {
     private let item: DetailModel
-    
-    init(
-        item: DetailModel
-    ) {
+
+    init(item: DetailModel) {
         self.item = item
     }
-    
+
     var body: some View {
         HStack {
             Spacer()
-            AsyncImage(
-                url: URL(
-                    string: item.info.thumb
-                )
-            ) { image in
-                image
-                    .resizable()
-                    .frame(
-                        width: 150,
-                        height: 100
-                    )
-            } placeholder: {
-                ProgressView()
+            VStack(spacing: 10) {
+                AsyncImage(url: URL(string: item.info.thumb)) { image in
+                    image
+                        .resizable()
+                        .frame(width: 150, height: 100)
+                } placeholder: {
+                    ProgressView()
+                }
+                
+                Button(action: {
+                    print("Add Tracking 버튼이 눌렸습니다.")
+                }) {
+                    Text("Add Tracking")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 150)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
             }
+            
             Spacer()
-            VStack(
-                alignment: .leading
-            ) {
-                Text(
-                    item.info.title
-                )
-                .font(
-                    .title2
-                )
-                .lineLimit(
-                    4
-                )
-                Text(
-                    "Retail Price: $\(item.deals.first?.retailPrice ?? "")"
-                )
-                Text(
-                    "Cheapest Ever: $\(item.cheapestPriceEver.price)"
-                )
+            VStack(alignment: .leading) {
+                Text(item.info.title)
+                    .font(.title2)
+                    .lineLimit(4)
+                Spacer()
+                    .frame(height: 20)
+                Text("Retail Price: $\(item.deals.first?.retailPrice ?? "")")
+                Text("Cheapest Ever: $\(item.cheapestPriceEver.price)")
             }
             Spacer()
         }
